751b06164455ca8994b7a54a9ceb0cdd718f4109
ksBluespire <kanishk.sugandhi@thebluespire.com>
2024-04-08 15:46:45 +0530

    verceljson1

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ AWS.config.update({
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
+// AWS.config.update({
+//   region: "us-east-1", // Update with your DynamoDB region
+//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+// });
+
 const docClient = new AWS.DynamoDB.DocumentClient();
 
 // Middleware
